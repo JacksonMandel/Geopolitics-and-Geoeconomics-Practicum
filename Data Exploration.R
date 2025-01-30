39e6a971f26207d255f52cdb2ac8eae71a9a44bb
Jackson Mandel <jm4344b@american.edu>
2025-01-30 18:21:27 -0500

    Update

--- Data Exploration.R
+++ Data Exploration.R
@@ -11,7 +11,7 @@ library(ggplot2)
 
 #Importing OECD Data on Intermediate, Final, and Gross Imports
 
-data <- readr::read_csv('Intermediate Final and Gross Imports.csv')
+data <- readr::read_csv('ImportData.csv')
 
 df_wide <- pivot_wider(data, names_from = MEASURE, values_from = OBS_VALUE)
 
@@ -23,7 +23,7 @@ df_wide$FinImpPer <- (df_wide$IMGR_FNL / df_wide$IMGR) * 100
 
 ggplot(df_wide, aes(x = TIME_PERIOD, y = FinImpPer, fill = "Finished Goods")) +
   geom_bar(stat = "identity", position = "dodge") +
-  geom_bar(aes(y = IntImpPer, fill = "Intermediate Goods"), stat = "identity") +
+  geom_bar(aes(y = IntImpPer, fill = "Intermediate Goods"), stat = "identity", position = "dodge") +
   facet_wrap(~ REF_AREA) +  # One panel per country
   labs(title = "Proportion of Finished and Intermediate Goods by Year",
        y = "Percentage of Total Imports",
@@ -31,3 +31,5 @@ ggplot(df_wide, aes(x = TIME_PERIOD, y = FinImpPer, fill = "Finished Goods")) +
        fill = "Import Type") +
   scale_fill_manual(values = c("Finished Goods" = "steelblue", "Intermediate Goods" = "orange")) +
   theme_minimal()
+
+writeLines(file_text, "DATA-EXPLORATION.Rmd", useBytes = TRUE)
